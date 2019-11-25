42538329856c3dd56aa84289e9d4a214a148a42d
mericturan <meric.turan@boun.edu.tr>
2019-11-25 21:34:51 +0300

    Implement Register Page (draft)

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { NotLoggedInLayout, PrivateLayout } from './layouts'
-import { HomeFeed, LoginPage, ProfilePage } from './containers'
+import { HomeFeed, LoginPage, RegistrationPage, ProfilePage } from './containers'
 
 const Routes = () =>
   <Router>
     <Switch>
       <NotLoggedInLayout component={HomeFeed} path='/' exact />
       <NotLoggedInLayout component={LoginPage} path='/login-page' exact />
+      <NotLoggedInLayout component={RegistrationPage} path='/register' exact />
       <PrivateLayout component={ProfilePage} path='/profile' exact />
     </Switch>
   </Router>
