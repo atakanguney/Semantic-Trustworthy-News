3752c3558b3a549754e50684fbbd61cb382ef3fd
atakanguney <atakanguney58@hotmail.com>
2019-12-10 18:41:02 +0300

    Implement test and getNewsBySlug

--- backend/models/news.js
+++ backend/models/news.js
@@ -2,6 +2,7 @@
 
 var sources = require('../models/sources')
 var authors = require('../models/authors')
+var slugify = require('slugify')
 var News = require('../models/neo4j/news')
 
 var createNews = async function ({
@@ -21,7 +22,7 @@ var createNews = async function ({
   var queries = [
     'MERGE (source:Source{id: {source_id}})',
     'MERGE (author:Author{id: {author_id}})',
-    'CREATE (news:NewsArticle {title: {title}, description: {description}, url: {url}, urlToImage: {urlToImage}, publishedAt: {publishedAt}, content: {content}})',
+    'CREATE (news:NewsArticle {title: {title}, description: {description}, url: {url}, urlToImage: {urlToImage}, publishedAt: {publishedAt}, content: {content}, slug: {slug}})',
     'CREATE (news)-[:hasSource]->(source)',
     'CREATE (news)-[:isWrittenBy]->(author)',
     'CREATE (author)-[:hasWritten]->(news)',
@@ -36,13 +37,25 @@ var createNews = async function ({
   url: url,
   urlToImage: urlToImage,
   publishedAt: publishedAt,
-  content: content
+  content: content,
+  slug: slugify(title)
 })
 		.then(results => {
   return new News(results.records[0].get('news'))
 })
 }
 
+var getNewsBySlug = function ({ session, slug }) {
+  return session.run('MATCH (news: NewsArticle{slug: {slug}}) RETURN news', { slug: slug }).then(results => {
+    if (results.records.length > 0) {
+      return new News(results.records[0].get(['news']))
+    } else {
+      return null
+    }
+  })
+}
+
 module.exports = {
-  createNews: createNews
+  createNews: createNews,
+  getNewsBySlug: getNewsBySlug
 }
--- backend/test/test-news.js
+++ backend/test/test-news.js
@@ -4,30 +4,45 @@ var dbUtils = require('../neo4j/dbUtils')
 var News = require('../models/news')
 
 describe('News', function () {
-  describe('#createNews', function () {
-    it('should return created news', function (done) {
-      params = {
-        source: {
-          id: 'wired',
-          name: 'Wired'
-        },
-        author: 'Laura Mallonee',
-        title: 'Inside the Icelandic Facility Where Bitcoin Is Mined',
-        description:
-					"Cryptocurrency mining now uses more of the Nordic island nation's electricity than its homes.",
-        url: 'https://www.wired.com/story/iceland-bitcoin-mining-gallery/',
-        urlToImage:
-					'https://media.wired.com/photos/5dbc37a4c955950008b26751/191:100/w_1280,c_limit/photo_barnard_explosions_4.jpg',
-        publishedAt: '2019-11-03T15:00:00Z',
-        content:
-					'Less than two miles from Icelands Reykjavik airport sits a nondescript metal building as monolithic and drab as a commercial poultry barn. Theres a deafening racket inside, too, but it doesnt come from clucking chickens. Instead, tens of thousands of whirring… [+3426 chars]'
-      }
+  var params = {
+    source: {
+      id: 'wired',
+      name: 'Wired'
+    },
+    author: 'Laura Mallonee',
+    title: 'Inside the Icelandic Facility Where Bitcoin Is Mined',
+    description: "Cryptocurrency mining now uses more of the Nordic island nation's electricity than its homes.",
+    url: 'https://www.wired.com/story/iceland-bitcoin-mining-gallery/',
+    urlToImage:
+			'https://media.wired.com/photos/5dbc37a4c955950008b26751/191:100/w_1280,c_limit/photo_barnard_explosions_4.jpg',
+    publishedAt: '2019-11-03T15:00:00Z',
+    content:
+			'Less than two miles from Icelands Reykjavik airport sits a nondescript metal building as monolithic and drab as a commercial poultry barn. Theres a deafening racket inside, too, but it doesnt come from clucking chickens. Instead, tens of thousands of whirring… [+3426 chars]',
+    slug: 'Inside-the-Icelandic-Facility-Where-Bitcoin-Is-Mined'
+  }
 
-      News.createNews({ session: dbUtils.getSession({}), ...params })
-				.then(response => {
+  after(function () {
+    dbUtils
+			.getSession({})
+			.run('MATCH (news: NewsArticle{slug: {slug}}) DETACH DELETE news', { slug: params['slug'] })
+			.then(results => {
+  console.log(results)
+})
+  })
+
+  it('should return created news', function (done) {
+    News.createNews({ session: dbUtils.getSession({}), ...params })
+			.then(response => {
   expect(response.title).to.equal(params['title'])
 })
-				.finally(done)
-    })
+			.finally(done)
+  })
+
+  it('should return news by slug', function (done) {
+    News.getNewsBySlug({ session: dbUtils.getSession({}), slug: params['slug'] })
+			.then(response => {
+  expect(response.slug).to.equal(params['slug'])
+})
+			.finally(done)
   })
 })
