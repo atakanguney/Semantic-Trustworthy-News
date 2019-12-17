337d9aef3610689f863eaf02feb29cee340eab4e
atakanguney <atakanguney58@hotmail.com>
2019-12-17 18:20:29 +0300

    Show a news

--- frontend/src/helpers/fetchNews.js
+++ frontend/src/helpers/fetchNews.js
@@ -3,3 +3,9 @@ import axios from 'axios'
 export const fetchNews = () => {
   return axios.get('/api/all-news')
 }
+
+export const getNewsBySlug = slug => {
+  return axios.get(`/api/news/${slug}`).then(response => {
+    return response.data
+  })
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { PublicLayout, PrivateLayout } from './layouts'
-import { HomeFeed, LoginPage, RegistrationPage, ProfilePage } from './containers'
+import { HomeFeed, LoginPage, RegistrationPage, ProfilePage, NewsItem } from './containers'
 
 const Routes = () =>
   <Router>
     <Switch>
       <PublicLayout component={HomeFeed} path='/' exact />
       <PublicLayout component={RegistrationPage} path='/register' exact />
+      <PublicLayout component={NewsItem} path='/news/:slug' />
       <PublicLayout component={LoginPage} path='/login' exact />
       <PrivateLayout component={ProfilePage} path='/profile' exact />
     </Switch>
